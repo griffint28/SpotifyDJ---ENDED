21d37f62c2dce9e5a8708d61f1e74410a826eac9
Lee Thomas Griffin <griffint1@g.cofc.edu>
2022-09-29 18:40:10 -0400

    Home Page Deployment

--- SpotifyDjSite/SpotifyDjApp/views.py
+++ SpotifyDjSite/SpotifyDjApp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import *
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.models import User
+from django.core.mail import send_mail as send_email
+from django.http import JsonResponse
+from django.conf import settings
+from .models import *
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+        return render(request=request, template_name= "../templates/SpotifyDjApp/homepage.html")
 # Create your views here.
+
+#SpotifyDjSite/SpotifyDjApp/templates/SpotifyDjApp/homepage.html
--- SpotifyDjSite/SpotifyDjSite/settings.py
+++ SpotifyDjSite/SpotifyDjSite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'SpotifyDjApp',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'SpotifyDjSite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +68,8 @@ TEMPLATES = [
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = 'SpotifyDjSite.wsgi.application'
 
