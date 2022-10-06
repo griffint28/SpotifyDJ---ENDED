96877608880201a9e5f2c2fe1800e00e170d7b1c
Lee Thomas Griffin <griffint1@g.cofc.edu>
2022-10-06 17:41:05 -0400

    Start test of Spotify Auth

--- SpotifyDjSite/SpotifyDjApp/views.py
+++ SpotifyDjSite/SpotifyDjApp/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.shortcuts import *
-from django.contrib.auth import authenticate, logout
-from django.contrib.auth import login as auth_login
-from django.contrib.auth.models import User
-from django.core.mail import send_mail as send_email
-from django.http import JsonResponse
-from django.conf import settings
 from .models import *
 
 def homepage(request):
