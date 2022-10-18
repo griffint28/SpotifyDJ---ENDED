382f95cff9d11f9e8fde0a6ec545d5656a9b8c42
jonesbd <82526338+jonesbd@users.noreply.github.com>
2022-10-18 12:02:47 -0400

    Add files via upload

--- SpotifyDjSite/SpotifyDjApp/urls.py
+++ SpotifyDjSite/SpotifyDjApp/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('', views.homepage, name='hompage'),
+    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    path('homepage', views.homepage, name='hompage'),
     path('login', views.login, name='login'),
     path('list', views.list, name='list'),
     path('spotifytest', views.spotifytest, name='spotifytest'),
-    path('spotifyplayer', views.spotifyplayer, name='spotifyplayer'),
+    path('test', views.test, name='test'),
+   
 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- SpotifyDjSite/SpotifyDjApp/views.py
+++ SpotifyDjSite/SpotifyDjApp/views.py
@@ -8,6 +8,11 @@ from spotipy import oauth2
 import requests
 
 
+
+
+def home(request):
+        return render(request=request, template_name= "../templates/SpotifyDjApp/home.html")
+
 def homepage(request):
         return render(request=request, template_name= "../templates/SpotifyDjApp/homepage.html")
 
@@ -34,15 +39,42 @@ def spotifytest(request):
 
         if auth_token:
                 sp = spotipy.Spotify(auth=auth_token['access_token'])
-                results = sp.current_user_top_tracks(limit=50,offset=0,time_range='medium_term')
+                results = sp.current_user_top_tracks(limit=10,offset=0,time_range='medium_term')
                 listTest = []
-                for i in range(50):
-                        listTest.append(results['items'][i]['name'] + " - " + results['items'][i]['album']['artists'][0]['name'] + "\n")
+                for i in range(10):
+                        listTest.append(results['items'][i]['name'] + " - " + results['items'][i]['album']['artists'][0]['name'] + "|")
                 return HttpResponse(listTest)
         else:
                 return render(request=request, template_name= "../templates/SpotifyDjApp/spotifytest.html")
 
-def spotifyplayer(request):
-        return render(request=request, template_name= "../templates/SpotifyDjApp/spotifyplayer.html")
+              
+def test(request):
+        cid = 'ee9fc019f133485296b33e83b6e674f9'
+        secret = '519f3c8ab9e646a5bdc484a6a643b2aa'
 
- 
+        sp_auth = oauth2.SpotifyOAuth(
+                client_id = cid,
+                client_secret = secret,
+                redirect_uri = "http://127.0.0.1:8008/SpotifyDjApp/spotify_callback",
+                scope = 'user-top-read'
+        )
+
+        redirect_url = sp_auth.get_authorize_url()
+        auth_token = sp_auth.get_access_token()  
+        print(auth_token['access_token'])
+
+        if auth_token:
+                sp = spotipy.Spotify(auth=auth_token['access_token'])
+                results = sp.current_user_top_tracks(limit=10,offset=0,time_range='medium_term')
+                listTest = []
+                print(results)
+                for i in range(10):
+                        listTest.append(results['items'][i]['name'] + " - " + results['items'][i]['album']['artists'][0]['name'])
+                return render(request, '../templates/SpotifyDjApp/test.html', {"list": listTest})
+        else:
+                return render(request=request, template_name= "../templates/SpotifyDjApp/test.html")
+
+def spotifyplayer(request):
+        return render(request=request)
+           
+              
